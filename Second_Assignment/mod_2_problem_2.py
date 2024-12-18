804fd3aad662937bb6187e0841bc8d095d7e10e2
rizzlebuck <aranda.nicholasb@gmail.com>
2024-12-17 20:54:57 -0800

    Add files via upload

---
+++ Second_Assignment/mod_2_problem_2.py
@@ -0,0 +1,29 @@
+# Task 1
+number_1 = float(input("Give me a number: "))
+number_2 = float(input("Give me a second number: "))
+number_3 = float(input("Good, now give me one more number: "))
+
+if number_1 >= number_2 and number_1 >= number_3:
+    print("Number one is largest")
+elif number_2 >= number_1 and number_2 >= number_3:
+    print("Number two is largest")
+else:
+    print("Number three is largest")
+
+    # Task 2
+number_1 = float(input("Give me a number: "))
+number_2 = float(input("Give me a second number: "))
+number_3 = float(input("Good, now give me one more number: "))
+
+if number_1 >= number_2 and number_1 >= number_3:
+    print("Number one is largest")
+elif number_2 >= number_1 and number_2 >= number_3:
+    print("Number two is largest")
+else:
+    print("Number three is largest")
+    if number_1 <= number_2 and number_1 <= number_3:
+        print("Number one is smallest")
+    elif number_2 <= number_1 and number_2 <= number_3:
+        print("Number two is smallest")
+    else:
+        print("Number three is smallest")
